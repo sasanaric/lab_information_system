53477a8c272a1e10db63b32fc7b5468b6c7fee4f
Sergej <sergej.s.soldat20@gmail.com>
2022-11-29 21:41:50 +0100

    initial commit

---
+++ src/main/java/lis/LabInfoSystemApplication.java
@@ -0,0 +1,13 @@
+package lis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabInfoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabInfoSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lis/LabInfoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabInfoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
